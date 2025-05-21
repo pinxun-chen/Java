acd95f8acd459380fb66f288d90a9b6b618c3c19
a9304 <a9304@DESKTOP-7VC3H1H>
2025-05-21 21:55:29 +0800

    home

---
+++ spring-projects/src/main/java/com/example/demo/model/dto/UserDto.java
@@ -0,0 +1,12 @@
+package com.example.demo.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+	private Integer id;
+	private String username;
+	private String email;
+	private Boolean completed;
+
+}
