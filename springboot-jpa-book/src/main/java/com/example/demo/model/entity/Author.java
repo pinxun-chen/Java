46c822f33beb409ad567cda575455c8e9a1ee70f
pinxun-chen <a930406a@gmail.com>
2025-05-26 10:15:44 +0800

    0526

--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Author.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Author.java
@@ -1,13 +1,26 @@
 package com.example.demo.model.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
 @Entity
 public class Author {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(length = 50, nullable = false)
 	private String name;
+	
+	@OneToMany(mappedBy = "author")
+	private List<Book> books;
 }
--- springboot-jpa-book/src/main/java/com/example/demo/model/entity/Book.java
+++ springboot-jpa-book/src/main/java/com/example/demo/model/entity/Book.java
@@ -1,13 +1,24 @@
 package com.example.demo.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(length = 100, nullable = false)
 	private String name;
+	
+	@ManyToOne
+	private Author author;
 }
