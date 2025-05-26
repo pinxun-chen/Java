d6fa9aec853e9fc2abb5eed5c0c1a2361fe54740
pinxun-chen <a930406a@gmail.com>
2025-05-26 11:25:22 +0800

    0526

--- springboot-jpa-book/src/test/java/com/example/demo/Test_Read.java
+++ springboot-jpa-book/src/test/java/com/example/demo/Test_Read.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.model.entity.Author;
 import com.example.demo.repository.AuthorRepository;
@@ -20,14 +21,17 @@ public class Test_Read {
 	private BookRepository bookRepository;
 
 	@Test
+	// @Transactional
 	public void read() {
-		// 查詢作者與書籍
+		// 查詢作者
 		List<Author> authors = authorRepository.findAll();
 		authors.forEach(author ->{
 			System.out.printf("ID:%d 姓名:%s %n", author.getId(), author.getName());
 		});
 		
-		authors.forEach(author ->{
+		// 查詢作者與書籍
+		List<Author> authors2 = authorRepository.findAllWithBooks();
+		authors2.forEach(author ->{
 			System.out.printf("ID:%d 姓名:%s 著作數量:%d%n", author.getId(), author.getName(), author.getBooks().size());
 		});
 	}
