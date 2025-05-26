f5f71f2f8ff34f20cf1829f0a80084ce6d0058a0
pinxun-chen <a930406a@gmail.com>
2025-05-26 11:07:36 +0800

    0526

---
+++ springboot-jpa-book/src/test/java/com/example/demo/Test_AddAuthor.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.entity.Author;
+import com.example.demo.repository.AuthorRepository;
+
+@SpringBootTest
+public class Test_AddAuthor {
+
+	@Autowired
+	private AuthorRepository authorRepository;
+	
+	@Test
+	public void addAuthor() {
+		Author author = new Author();
+		// author.setId(2);
+		author.setName("黃小華");
+		
+		authorRepository.save(author);
+	}
+}
