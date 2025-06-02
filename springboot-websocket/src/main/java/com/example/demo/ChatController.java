d0138e617d6bfc2260b19f705968df4d51439967
pinxun-chen <a930406a@gmail.com>
2025-06-02 11:42:28 +0800

    0602

--- springboot-websocket/src/main/java/com/example/demo/ChatController.java
+++ springboot-websocket/src/main/java/com/example/demo/ChatController.java
@@ -7,8 +7,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChatController {
 	@MessageMapping("/chat") // 有訊息到 /app/chat
-	@SendTo("/topic/message") // 訂閱一個主題:/topic, 主題叫做:/messages
+	@SendTo("/topic/messages") // 訂閱一個主題:/topic, 主題叫做:/messages
 	public ChatMessage send(ChatMessage message) {
+		return message;
+	}
+	
+	@MessageMapping("/chat2") // 有訊息到 /app/chat2
+	@SendTo("/topic/messages2") // 訂閱一個主題:/topic, 主題叫做:/messages2
+	public ChatMessage send2(ChatMessage message) {
 		return message;
 	}
 }
