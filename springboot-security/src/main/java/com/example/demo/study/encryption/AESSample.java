74c844f65425d108b8e3e4c7095483c725391500
pinxun-chen <a930406a@gmail.com>
2025-06-11 12:11:44 +0800

    0611

--- springboot-security/src/main/java/com/example/demo/study/encryption/AESSample.java
+++ springboot-security/src/main/java/com/example/demo/study/encryption/AESSample.java
@@ -13,7 +13,7 @@ import com.example.demo.study.security.KeyUtil;
 public class AESSample {
 	
 	// 建立一個 AES Key(256bits, 32bytes)
-	private static final String KEY = "012345678901234567890123456789AB"; // 32 個字
+	// private static final String KEY = "012345678901234567890123456789AB"; // 32 個字
 	
 	public static void main(String[] args) throws Throwable {
 		String originalText = "今天是好天氣"; // 明文
